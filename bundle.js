f5b609f82e9a2ff1f7cc9a2097874950734be356
JS-Ninja <raviroshan.work@gmail.com>
2018-08-25 10:50:15 +0530

    Initial Commit with seperate files

---
+++ bundle.js
@@ -0,0 +1,9 @@
+// File 1
+console.log('File 1 Loaded');
+
+var userName = 'Prabhu';
+
+document.getElementById('user-name').innerHTML = userName;
+
+// File 2
+console.log('File 2 Loaded : ', userName);
---
+++ file1.js
@@ -0,0 +1,5 @@
+console.log('File 1 Loaded');
+
+var userName = 'Prabhu';
+
+document.getElementById('user-name').innerHTML = userName;
